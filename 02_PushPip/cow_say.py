d403bcae8a85952f8dd48dda51f8215601e3d414
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-02-22 22:42:47 +0300

    [02_PushPip] Add support for cow files
    A cow file can be built-in (-l to list available) or custom *.cow.

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -11,16 +11,48 @@ for p in "dead", "greedy", "paranoid", "stoned", "tired", "wired", "youthful":
 # Parsing eyes
 parser.add_argument("-e", default="oo",
                     help="specify eyes appearance", metavar="eyes")
+# Parsing cow file
+parser.add_argument("-f", help="specify cow picture file (built-in or custom)",
+                    metavar="cowfile")
+# Showing cows list
+parser.add_argument("-l", action="store_true",
+                    help="show list of built-in cow files and exit")
 # Parsing tongue
 parser.add_argument("-T", default='',
                     help="specify tongue appearance", metavar="tongue")
 
 args = parser.parse_args()
+avail = sorted(cowsay.list_cows())
 preset = ''.join(p for p in "bdgpstwy" if getattr(args, p))
 
-print(cowsay.cowsay(
-    "Hello world!",
-    preset=preset,
-    eyes=args.e,
-    tongue=args.T
-))
+if args.l:
+    print("Built-in cow files:")
+    print(*avail)
+elif args.f is None:
+    # cow file not specified
+    print(cowsay.cowsay(
+        "Hello world!",
+        preset=preset,
+        eyes=args.e,
+        tongue=args.T
+    ))
+elif args.f in avail:
+    # cow file is built-in
+    print(cowsay.cowsay(
+        "Hello world!",
+        cow=args.f,
+        preset=preset,
+        eyes=args.e,
+        tongue=args.T
+    ))
+else:
+    # cow file is custom *.cow
+    with open(args.f) as f:
+        cowfile = cowsay.read_dot_cow(f)
+    print(cowsay.cowsay(
+        "Hello world!",
+        preset=preset,
+        eyes=args.e,
+        tongue=args.T,
+        cowfile=cowfile
+    ))
