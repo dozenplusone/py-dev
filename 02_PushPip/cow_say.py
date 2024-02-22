839bea234d07c7afbea6caffd931a60e6e0c186a
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-02-22 22:11:25 +0300

    [02_PushPip] Add support for presets
    The presets are original ones: -[bdgpstwy]. Mutex is intentional.

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -3,5 +3,16 @@ import cowsay
 
 
 parser = argparse.ArgumentParser()
+# Parsing presets
+presets = parser.add_argument_group("presets").add_mutually_exclusive_group()
+presets.add_argument("-b", action="store_true", help="Borg")
+for p in "dead", "greedy", "paranoid", "stoned", "tired", "wired", "youthful":
+    presets.add_argument(f"-{p[0]}", action="store_true", help=p)
+
 args = parser.parse_args()
-print(cowsay.cowsay("Hello world!"))
+preset = ''.join(p for p in "bdgpstwy" if getattr(args, p))
+
+print(cowsay.cowsay(
+    "Hello world!",
+    preset=preset
+))
