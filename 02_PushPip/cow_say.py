3006b50e6e09c13265f45a7ae30bd61f2758528c
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-02-22 22:20:47 +0300

    [02_PushPip] Add support for custom eyes and tongue

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -8,11 +8,19 @@ presets = parser.add_argument_group("presets").add_mutually_exclusive_group()
 presets.add_argument("-b", action="store_true", help="Borg")
 for p in "dead", "greedy", "paranoid", "stoned", "tired", "wired", "youthful":
     presets.add_argument(f"-{p[0]}", action="store_true", help=p)
+# Parsing eyes
+parser.add_argument("-e", default="oo",
+                    help="specify eyes appearance", metavar="eyes")
+# Parsing tongue
+parser.add_argument("-T", default='',
+                    help="specify tongue appearance", metavar="tongue")
 
 args = parser.parse_args()
 preset = ''.join(p for p in "bdgpstwy" if getattr(args, p))
 
 print(cowsay.cowsay(
     "Hello world!",
-    preset=preset
+    preset=preset,
+    eyes=args.e,
+    tongue=args.T
 ))
