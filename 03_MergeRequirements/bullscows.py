92cd306d8171e932cf0f739062a592f1408b14e2
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-03-04 21:22:13 +0300

    [03] Add gameplay func

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,3 +1,6 @@
+import random
+
+
 def bullscows(guess: str, secret: str) -> tuple[int, int]:
     bulls, cows = [], []
     for i, c in enumerate(guess):
@@ -18,3 +21,13 @@ def ask(prompt: str, valid: list[str] = None) -> str:
 
 def inform(format_string: str, bulls: int, cows: int) -> None:
     print(format_string.format(bulls, cows))
+
+
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    secret = random.choice(words)
+    ctr = 1
+    while (guess := ask("Введите слово: ", words)) != secret:
+        inform("Быки: {}, Коровы: {}", *bullscows(guess, secret))
+        ctr += 1
+    inform("Быки: {}, Коровы: {}", *bullscows(guess, secret))
+    return ctr
