db0193af638044393560e79293d454c2ea2955af
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-03-04 20:58:51 +0300

    [03] Create ask & inform funcs
    ask reads a line and checks it for validity if needed.
    inform prints a formatted string with info on bulls and cows.

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -6,3 +6,15 @@ def bullscows(guess: str, secret: str) -> tuple[int, int]:
         elif c in secret:
             cows.append(i)
     return len(bulls), len(cows)
+
+
+def ask(prompt: str, valid: list[str] = None) -> str:
+    ans = input(prompt)
+    if valid is not None:
+        while ans not in valid:
+            ans = input(prompt)
+    return ans
+
+
+def inform(format_string: str, bulls: int, cows: int) -> None:
+    print(format_string.format(bulls, cows))
