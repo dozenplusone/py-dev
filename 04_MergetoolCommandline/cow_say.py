2480f28e4bbdc9a627a20225e2662076df7ec879
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-03-11 15:14:40 +0300

    [04] Add help text for all cmds

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -11,16 +11,24 @@ def parseArgs(s):
 
 class CowShell(cmd.Cmd):
     def do_list_cows(self, arg):
+        """list_cows
+        List available cows."""
         print("Built-in cow files:")
         print(*sorted(cowsay.list_cows()))
 
     def do_make_bubble(self, arg):
+        """make_bubble text TEXT
+        Make a cowsay's bubble containing TEXT."""
         print(cowsay.make_bubble(**parseArgs(arg)))
 
     def do_cowsay(self, arg):
+        """cowsay message MESSAGE [eyes EYES] [tongue TONGUE] [cow COW]
+        Draw a COW with EYES and TONGUE saying MESSAGE."""
         print(cowsay.cowsay(**parseArgs(arg)))
 
     def do_cowthink(self, arg):
+        """cowthink message MESSAGE [eyes EYES] [tongue TONGUE] [cow COW]
+        Same as cowsay (see: help cowsay), but with different bubble style."""
         print(cowsay.cowthink(**parseArgs(arg)))
 
 if __name__ == '__main__':
