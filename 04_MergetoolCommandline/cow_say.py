9f4b1d86460ea217bcae045f40b83db88db77911
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-03-11 14:59:10 +0300

    [04] Add argument parser

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -3,6 +3,12 @@ import cowsay
 import shlex
 
 
+def parseArgs(s):
+    args = shlex.split(s)
+    assert len(args) & 1 == 0
+    return dict(zip(args[::2], args[1::2]))
+
+
 class CowShell(cmd.Cmd):
     def do_list_cows(self, arg):
         print("Built-in cow files:")
