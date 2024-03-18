a388cc56c81e8a571e4831d5974b6b5c51178536
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-03-18 19:17:15 +0300

    Add core chatting code and placeholder for cmd executer

--- 05_DiffPatchNet/server.py
+++ 05_DiffPatchNet/server.py
@@ -6,7 +6,30 @@ clients = {}
 
 
 async def cowChat(reader, writer):
-    pass
+    async def cmdExec(cmd: str):
+        nonlocal buffer
+        await buffer.put(cmd)
+
+    buffer = asyncio.Queue()
+    send = asyncio.create_task(reader.readline())
+    receive = asyncio.create_task(buffer.get())
+    while not reader.at_eof():
+        done, pending = await asyncio.wait(
+            [send, receive],
+            return_when=asyncio.FIRST_COMPLETED
+        )
+        for task in done:
+            if task is send:
+                send = asyncio.create_task(reader.readline())
+                await cmdExec(task.result().decode())
+            elif task is receive:
+                receive = asyncio.create_task(buffer.get())
+                writer.write(f"{task.result()}\n".encode())
+                await writer.drain()
+    send.cancel()
+    receive.cancel()
+    writer.close()
+    await writer.wait_closed()
 
 
 async def main():
