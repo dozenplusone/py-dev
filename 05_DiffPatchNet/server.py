df45ee86f14b0ec2c4cb1561350a1a2fe6428a35
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-03-18 18:17:17 +0300

    Add code for running server and placeholder for main code

--- 05_DiffPatchNet/server.py
+++ 05_DiffPatchNet/server.py
@@ -1,2 +1,18 @@
 import asyncio
 import cowsay
+
+
+clients = {}
+
+
+async def cowChat(reader, writer):
+    pass
+
+
+async def main():
+    server = await asyncio.start_server(cowChat, "localhost", 1337)
+    async with server:
+        await server.serve_forever()
+
+
+asyncio.run(main())
