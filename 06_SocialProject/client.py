977c835aac84e5fe06313fb0c5066da1f6834e32
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-03-25 11:34:43 +0300

    Add support for 'who' cmd

--- 06_SocialProject/client.py
+++ 06_SocialProject/client.py
@@ -11,6 +11,13 @@ class ChatCmd(cmd.Cmd):
         self.sockfd.sendall(f"login {arg.split()[0]}\n".encode())
         print(self.sockfd.recv(1024).decode().rstrip())
 
+    def do_who(self, arg):
+        if arg:
+            print("error: 'who' takes no arguments")
+        else:
+            self.sockfd.sendall(b"who\n")
+            print(self.sockfd.recv(1024).decode().rstrip())
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sockfd:
     sockfd.connect(("localhost", 1337))
